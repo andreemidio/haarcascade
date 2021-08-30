ab2b099c821d3043e198228ca488f36727e099f1
Andre Luiz Emidio Junior <andre.junior@engdb.com.br>
2021-08-29 22:00:44 -0300

    Criação do projeto base

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+
+
+cv::CascadeClassifier face_cascade;
+
+int main() {
+
+    cv::VideoCapture capture;
+    int camera_device = 1;
+    cv::Mat frame;
+
+    std::cout << "Hello, World!" << std::endl;
+
+
+    capture.open(camera_device, cv::CAP_DSHOW);
+
+    if (!capture.isOpened()) {
+        std::cout << "Erro ao abrir o video" << std::endl;
+        return 0;
+
+    }
+
+    while (capture.read(frame)) {
+
+        if (!frame.empty()) {
+            std::cout << "Não capturou frame" << std::endl;
+            break;
+        }
+
+        cv::imshow("Frame", frame);
+
+
+        if (cv::waitKey(10) == 27) {
+            break;
+        }
+    }
+
+
+    return 0;
+}
